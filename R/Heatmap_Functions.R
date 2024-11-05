2acd020cbbb2821c53a7b9863cf1b3e96503e89b
Radhika Kulkarni <rrkulkarni@tamu.edu>
2024-11-05 10:01:25 -0600

    move the R file into R folder

---
+++ R/Heatmap_Functions.R
@@ -0,0 +1,73 @@
+# Functions that visualize spatiotemporal data using heatmaps
+
+# Function that subsets the data and reformats to be appropriate input for other functions
+###############################################################################################
+# Description of supplied parameters:
+# X - n x p training data
+# y - a vector of size n of class labels
+# OUTPUT
+
+SubsetData <- function(X){
+}
+
+
+
+## Return output
+##########################################################################
+
+
+
+
+# Function that assigns clusters and reformats to be appropriate input for other functions
+###############################################################################################
+# Description of supplied parameters:
+# X - n x p training data
+# y - a vector of size n of class labels
+# OUTPUT
+
+AssignClusters <- function(X){
+
+
+}
+
+
+
+## Return output
+##########################################################################
+
+
+# Function that creates heatmap from the data using other helper functions
+###############################################################################################
+# Description of supplied parameters:
+# X - n x p training data
+# y - a vector of size n of class labels
+# OUTPUT
+
+CreateHeatmap <- function(X){
+
+
+}
+
+
+
+## Return output
+##########################################################################
+
+
+
+# Function that plots data and allows for more control over plotting parameters
+###############################################################################################
+# Description of supplied parameters:
+# X - n x p training data
+# y - a vector of size n of class labels
+# OUTPUT
+
+PlotHeat <- function(X){
+
+
+}
+
+
+
+## Return output
+##########################################################################
