bbb0b0a93375cd00d3d6959582cc4138d760eaae
Radhika Kulkarni <rrkulkarni@tamu.edu>
2024-11-09 23:58:07 -0600

    add function which flattens dataset into workable form for time series

--- R/ENSO.R
+++ R/ENSO.R
@@ -18,8 +18,36 @@ library(readxl)
 # Load the data
 enso_data <- read_excel("Data/ENSO.xlsx")
 
+## Function takes in dataframe from any timepoint of this website (it is updated frequently) https://origin.cpc.ncep.noaa.gov/products/analysis_monitoring/ensostuff/ONI_v5.php
+## StartDate string value for season start date "year1-year2"
+## EndDate string value for season end date "year19-year20"
 findDateSubset <- function(dataframe, StartDate, EndDate) {
   dataframe <- dataframe %>%
     filter(Season >= StartDate & Season <= EndDate)
   return(dataframe)
 }
+#findDateSubset(enso_data, "2001-2002", "2020-2021") test that subset function works
+
+# Reshape the data into a long format
+
+makeLongForm <- function(dataframe) {
+
+enso_long <- enso_data %>%
+  pivot_longer(
+    cols = -c("ENSO Type", "Season"),
+    names_to = "Month",
+    values_to = "Values"
+  ) %>%
+  mutate(
+    YrMon = paste(Season, Month, sep = "-"), # Create a Year-Month column
+    YrMon = factor(YrMon, levels = unique(YrMon)) # Ensure order is preserved
+  )
+
+# Display a few rows for verification
+head(enso_long)
+
+return (enso_long)
+
+}
+
+makeLongForm(enso_data)
