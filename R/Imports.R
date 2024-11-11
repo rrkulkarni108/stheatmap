e284202d8b3e74fa230d5acae0a5275708defaaa
Radhika Kulkarni <rrkulkarni@tamu.edu>
2024-11-10 21:22:39 -0600

    update Imports.R files to update NAMESPACE

--- R/Imports.R
+++ R/Imports.R
@@ -1,4 +1,10 @@
 #' Import functions to update NAMESPACE
+#' @name Imports
+#' @title Imports for stheatmap package
+#' @import dplyr
+#' @import tidyr
+#' @import readxl
+#' @import lubridate
 #' @import ggplot2
 #' @importFrom stats hclust
 #' @import cluster
